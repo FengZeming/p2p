1919cb78b23bcb71b33de536a957a411e11f01fc
guider <guider@yeah.net>
2017-10-27 11:57:17 +0800

    fix axios post

--- src/pages/detail/PlatDetail.vue
+++ src/pages/detail/PlatDetail.vue
@@ -1,23 +1,33 @@
 <template>
-  <div>
-    detail
-  </div>
+  <view-box class="viewBox" style="margin-bottom: 0;">
+    <div v-for="item,index in detail">
+      <cell class="cell" :message='{item:item ,index:index }'></cell>
+
+    </div>
+  </view-box>
 </template>
 
 <script>
   import fetch from '../../api/http'
+  import Cell from './components/DetailHeaderCell'
+  import SymbolBar from '../../components/SymbolBar'
+  import {ViewBox} from 'vux'
 
   export default {
 
     name: 'detail',
-    date() {
-      return {}
+    components: {Cell, ViewBox},
+    data() {
+      return {
+        detail: {}
+      }
     },
     mounted() {
       console.log(this.$route.query);
       fetch('/table/platformDetail', {type: 'post', params: {'platid': this.$route.query.platid}})
-        .then(function (response) {
-          console.log(response)
+        .then((response) => {
+          this.detail = response.data;
+          console.log(this.detail)
         }).catch(function (err) {
         console.log(err)
       })
@@ -25,3 +35,8 @@
 
   }
 </script>
+
+<style scoped>
+
+
+</style>
---
+++ src/pages/detail/components/DetailHeaderCell.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+
+  </div>
+
+</template>
+
+<script></script>
+
+<style scoped>
+  .container {
+    background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#2593fb), to(#0554c6));
+    height: 130px;
+    width: 100%;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    /*-moz-transform:rotate(45deg);*//*-webkit-transform:rotate(45deg);*//*-o-transform:rotate(45deg);  *//*-ms-transform:rotate(45deg); */
+    flex-direction: column;
+    transform: rotate(0deg);
+  }
+
+</style>
