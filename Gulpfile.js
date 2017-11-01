ff496527a9261ccbaa719ea8ff437e91e1adb81c
guider <guider@yeah.net>
2017-11-01 11:46:23 +0800

    gulp config

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,24 +2,16 @@ var gulp = require('gulp');
 var sftp=require('gulp-sftp')
 var ssh=require('gulp-ssh')
 
-var config = {
-  sftp: {
-    host: '',
-    user: '',
-    port: '',
-    key: '',
-    remotePath: ''
-  }
-};
+var config = require('./ftpconfig.js')
 
 //上传到远程服务器任务
 gulp.task('upload', function () {
-  return gulp.src('./build/**')
+  return gulp.src('/Users/guider/Documents/vue/p2p/dist/**')
     .pipe(sftp({
       host: config.sftp.host,
-      user: config.sftp.user,
       port: config.sftp.port,
-      key: config.sftp.key,
+      user: config.sftp.user,
+      pass: config.sftp.key,
       remotePath: config.sftp.remotePath
     }));
 });
