8848e8297d3b10b084b09935d9f4aaa774e886db
guider <guider@yeah.net>
2017-11-01 19:35:05 +0800

    back up

--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    Me
+    <me-header-cell>
+    </me-header-cell>
+    <me-wealth-cell style="margin-top: 5px;">
+
+    </me-wealth-cell>
+    <divider :message="{height:'15px'}"></divider>
+
   </div>
 </template>
+
+<script>
+  import MeHeaderCell from './components/MeHeaderCell'
+  import MeWealthCell from './components/MeWealthCell'
+  import Divider from '../../components/Divider'
+
+  export default {
+    components: {
+      MeHeaderCell,
+      MeWealthCell,
+      Divider
+    }
+
+  }
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/me/components/MeHeaderCell.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="background-image: url(https://i.loli.net/2017/11/01/59f9ae238ae93.png)">
+    <div style="display: flex;width: 100%;height: auto;">
+      <img :src="require('../../../assets/images/钱包.png')" alt="头像">
+      <div style="align-self: center;margin-left: -5px;">
+        <p style="color: white;font-size: 17px;">miss古铜</p>
+        <p style="color: white;font-size: 12px;">黄金会员</p>
+      </div>
+
+
+    </div>
+    <div
+      style="display: flex; background-color: dodgerblue;justify-content: center;align-items: center;align-self: center;">
+      <p style="color: white;font-size: 16px;padding: 5px; flex: 1;align-self: center;">可提现金额: 10.36 </p>
+      <button>提现</button>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  img {
+    width: 44px;
+    height: 44px;
+    margin: 20px;
+    background-color: black;
+    border-radius: 22px;
+  }
+
+  button {
+    background-color: white;
+    border-radius: 2px;
+    border-color: darkblue;
+    margin-top: 6px;
+    margin-bottom: 6px;
+    font-size: 15px;
+    color: blue;
+    margin-right: 10px;
+    padding: 2px 10px;
+  }
+
+</style>
