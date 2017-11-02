4cd8332f3507ac82cbcba73b6beae5febb0bd9b5
guider <guider@yeah.net>
2017-11-02 17:53:14 +0800

    scroll demo

--- src/pages/attention/Attention.vue
+++ src/pages/attention/Attention.vue
@@ -1,7 +1,56 @@
 <template>
+  <div style="width: 100%;height: 100%;display: flex;">
+    <symbol-bar ref="sybolBar" style="position: fixed;"></symbol-bar>
 
-  <div>
-     我的关注
+    <div style="display: flex;flex-direction: row; padding-top: 50px;height: 44px;width: 100%; align-items: center;">
+
+
+      <div style="min-width: 100px;display: flex;flex-direction: column;">
+
+        <div v-for="ix in 10">
+          <div style="display:flex; align-items: center;justify-content: center; font-size: 15px;width: 30%;">
+            侧边栏
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="overflow: scroll; " :style="{height:10*100+'px'}" ref="gridContentContainer">
+      <div v-for="ixxx in 10">
+        <div
+          style="display: flex;flex-direction: row; padding-top: 50px;width: 100%;  align-items: center;flex: 1;">
+          <div style="min-width: 100px;display: flex;flex-direction: row;">
+            <div v-for="ix in 10">
+              <div style="display:flex; align-items: center;justify-content: center; font-size: 15px;min-width: 100px;">
+                内容
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
-
 </template>
+<script>
+  import SymbolBar from './components/AttentionSymbolBar.vue'
+
+  export default {
+
+    components: {
+      SymbolBar
+    },
+    methods: {
+      scrollLeft() {
+        console.log(this.$refs.gridContentContainer.scrollLeft);
+        this.$refs.sybolBar.scrollLeftTo(this.$refs.gridContentContainer.scrollLeft)
+      }
+    },
+    mounted() {
+
+      this.$refs.gridContentContainer.addEventListener('scroll', this.scrollLeft)
+
+    }
+  }
+
+</script>
---
+++ src/pages/attention/components/AttentionSymbolBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div style="display: flex;flex-direction: row;
+    height: 44px;width: 100%; align-items: center;">
+    <p style="display:flex; align-items: center;justify-content: center; font-size: 15px;width: 20%;">
+      {{indexTitle(0)}}
+    </p>
+    <div style="display: flex;overflow: scroll; width: 80%;" ref="gridHeaderContainer">
+      <p class="wrapItem" style="margin-left: 0;">
+        indexTitle(1)indexTitleindexTitleindexTitle
+      </p>
+      <p class="wrapItem">
+        indexTitle(1)indexTitleindexTitleindexTitle
+      </p>
+      <p class="wrapItem">
+        {{indexTitle(3)}}
+      </p>
+      <p class="wrapItem">
+        {{indexTitle(4)}}
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['message'],
+
+    methods: {
+      indexTitle(i) {
+        return '12';
+        return this.message[i];
+      },
+      titleLength() {
+        return this.message.length() / 5;
+      },
+      menu() {
+        console.log (' scroll  :  '+this.$refs.gridHeaderContainer.scrollLeft)
+      },
+      scrollLeftTo(x){
+        console.log (' scrollTo:' + x);
+        this.$refs.gridHeaderContainer.scrollTo(x,100);
+      }
+    },
+    computed: {},
+    mounted() {
+      this.$refs.gridHeaderContainer.addEventListener('scroll',this.menu);
+    },
+  }
+</script>
+<style scoped>
+  .wrapItem {
+    width: 400px;
+    margin-left: 4px;
+    margin-right: 4px;
+    font-size: 14px;
+    text-align: center;
+    color: #333;
+  }
+
+</style>
