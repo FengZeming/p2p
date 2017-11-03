a4b4761e62614f4f419ec4979ecd10e4257620c8
guider <guider@yeah.net>
2017-11-03 19:28:52 +0800

    sign ranking

--- src/pages/signPages/signScore/SignScore.vue
+++ src/pages/signPages/signScore/SignScore.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="vux-circle-demo">
-    <div style='width:150px;height:150px;'>
-      <x-circle :percent="percent2" :stroke-width="10" :trail-width="7" :stroke-color="strokeColor2" trail-color="#ececec">
-        <span :style="{color: strokeColor2}">{{percent2}}%</span>
-      </x-circle>
+  <div>
+    <sign-score-header-cell></sign-score-header-cell>
+    <p>
+      继续加油!<br/>比25%的小伙伴懂得多
+    </p>
+    <div class="vux-circle-demo">
+      <div style='width:150px;height:150px;'>
+        <x-circle :percent="percent2" :stroke-width="10" :trail-width="7" :stroke-color="strokeColor2"
+                  trail-color="#ececec">
+          <span :style="{color: strokeColor2}">{{percent2}}%</span>
+        </x-circle>
+      </div>
     </div>
+
+
+
   </div>
+
 </template>
 
 <script>
-  import { XCircle, Range, Icon } from 'vux'
+  import {XCircle, Range, Icon} from 'vux'
+  import SignScoreHeaderCell from './components/SignScoreHeaderCell'
 
   export default {
-    ready () {
+    ready() {
       setInterval(this.update2, 2000)
     },
     components: {
       XCircle,
       Range,
-      Icon
+      Icon,
+      SignScoreHeaderCell
     },
-    data () {
+    data() {
       return {
         percent1: 10,
         percent2: 60,
@@ -28,7 +41,7 @@
       }
     },
     methods: {
-      update2 () {
+      update2() {
         const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A']
         this.percent2 = parseInt(Math.random() * 100, 10)
         this.strokeColor2 = colorMap[parseInt(Math.random() * 3, 10)]
@@ -41,10 +54,26 @@
   .vux-circle-demo {
     text-align: center;
   }
+
   .vux-circle-demo > div {
     margin: 0 auto;
   }
-  span{
+
+  span {
     font-size: 32px;
+  }
+
+  p {
+    width: 398px;
+    height: 82px;
+    margin-top: 30px;
+    font-family: ﻿FZLTHK--GBK1-0;
+    font-size: 18px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    letter-spacing: 0px;
+    text-align: center;
+    color: #448aff;
   }
 </style>
---
+++ src/pages/signPages/signScore/components/SignScoreHeaderCell.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="header-container">
+      <div style="flex: 1; width: 100%;display: flex;margin: 0;flex-direction: column;">
+        <p style="margin-top: 16px;color: white;font-size: 20px;">第 <span style="font-size: 39px;">4567</span> 名</p>
+      </div>
+    </div>
+    <div style="margin-left: 0;margin-right: 0; width: 100%;height: 40px;display: flex;  align-items: center;">
+      <img>
+      <p style="color: #666;font-size: 13px;">
+        恭喜发财大吉大利
+      </p>
+    </div>
+  </div>
+</template>
+<style scoped>
+  .header-container {
+    background: url("../../../../assets/images/sign/脑容量.png") no-repeat center center;
+    width: 100%;
+    height: 22vh;
+    background-size: 100% 22vh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+  }
+
+  .header-container div {
+    height: 40px;
+    width: 100%;
+    margin-bottom: 20px;
+    padding-left: 12px;
+    margin-right: 12px;
+    display: flex;
+    align-items: center;
+  }
+
+  img {
+    background: black;
+    width: 40px;
+    height: 40px;
+    margin-left: 12px;
+    margin-right: 12px;
+    border-radius: 20px;
+
+  }
+
+  .header-container div p {
+    color: #666;
+    font-size: 14px;
+    text-align: start;
+    flex: 1;
+    margin-left: 10px;
+  }
+</style>
