09e796bc1d49178a76146d3b5eb7080085ab25b5
guider <guider@yeah.net>
2017-11-17 18:28:48 +0800

    fix qrcode style

--- src/pages/qrcode/Qrcode.vue
+++ src/pages/qrcode/Qrcode.vue
@@ -1,21 +1,26 @@
 <template>
-  <div style="display: flex;flex-direction: row;">
-    <tab :line-width="1" custom-bar-width="60px" style="position: fixed;width:100%;">
-      <tab-item selected @on-item-click="onItemClick(0)">学前</tab-item>
-      <tab-item @on-item-click="onItemClick(1)">1-2年级</tab-item>
-      <tab-item @on-item-click="onItemClick(2)">3-4年级</tab-item>
-      <tab-item @on-item-click="onItemClick(3)">5-6年级</tab-item>
-      <tab-item @on-item-click="onItemClick(4)">初中</tab-item>
-      <tab-item @on-item-click="onItemClick(5)">高中</tab-item>
+  <div style="display: flex;flex-direction: column;">
+    <tab v-show="!selectedType" :line-width="1" custom-bar-width="70px" style="position: relative;width:100%;">
+      <tab-item :selected="isTabSelect(0)" @on-item-click="onItemClick(0)">学前</tab-item>
+      <tab-item :selected="isTabSelect(1)" @on-item-click="onItemClick(1)">小学</tab-item>
+      <tab-item :selected="isTabSelect(4)" @on-item-click="onItemClick(4)">初中</tab-item>
+      <tab-item :selected="isTabSelect(5)" @on-item-click="onItemClick(5)">高中</tab-item>
     </tab>
-    <div style="padding-top: 50px;align-items:center;display: flex;flex-direction: column;width: 100%;">
+    <tab v-if="showChildTab" :line-width="1" custom-bar-width="60px" :style="{width:selectedType? '100%':'80%'}"
+         style="position: relative;height: 38px;align-self: center;">
+      <tab-item :selected="isChildTabSelect(1)" @on-item-click="onItemClick(1)">1-2年级</tab-item>
+      <tab-item :selected="isChildTabSelect(2)" @on-item-click="onItemClick(2)">3-4年级</tab-item>
+      <tab-item :selected="isChildTabSelect(3)" @on-item-click="onItemClick(3)">5-6年级</tab-item>
+    </tab>
+    <div style="padding-top: 50px;align-items:center;display: flex;flex-direction: column; width: 100%;"
+    >
       <p class="btn"> 长按二维码，加入家长交流群 </p>
       <img v-if="selectedIndex==0" :src="type0.src">
       <img v-if="selectedIndex==1" :src="type1.src">
       <img v-if="selectedIndex==2" :src="type2.src">
       <img v-if="selectedIndex==3" :src="type3.src">
-      <img v-if="selectedIndex==4" :src="type3.src">
-      <img v-if="selectedIndex==5" :src="type3.src">
+      <img v-if="selectedIndex==4" :src="type4.src">
+      <img v-if="selectedIndex==5" :src="type5.src">
       <p class="hint">还剩{{residue}}个免费名额，快快扫码入群吧！</p>
 
     </div>
@@ -72,18 +77,34 @@
             .then(function (response) {
               self['type' + self.selectedIndex] = response.data.data
             }).catch(function (err) {
-            console.log(err);
           })
         }
+      },
+      isTabSelect(index) {
+        if (this.selectedIndex == 1 || this.selectedIndex == 2 || this.selectedIndex == 3) {
+          return index == 1;
+        }
+        return this.selectedIndex == index;
+      },
+      isChildTabSelect(index) {
+        return this.selectedIndex == index;
       }
     },
     computed: {
       residue() {
         return this['type' + this.selectedIndex].count;
+      },
+      showChildTab() {
+        return this.selectedIndex == 1 || this.selectedIndex == 2 || this.selectedIndex == 3;
+      },
+      selectedType() {
+        return (this.$route.query && this.$route.query.selectedType == 'primary')
       }
     },
     mounted() {
       this.onItemClick(0);
+      this.selectedIndex = this.$route.query.selectedIndex;
+      console.log(this.$route.query)
     }
   }
 
