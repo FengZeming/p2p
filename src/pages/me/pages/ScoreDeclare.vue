29d45ef2c77f3cce0f77aaa0388cdf2d6b94abe8
guider <guider@yeah.net>
2017-11-23 00:11:39 +0800

    fix style

--- src/pages/me/pages/ScoreDeclare.vue
+++ src/pages/me/pages/ScoreDeclare.vue
@@ -22,18 +22,17 @@
 	letter-spacing: 0px;
 	margin-left: 10px;
 	margin-right: 20px;
-
 	color: #666;"></p>
     </div>
-    <div v-for="item ,index in table " style="margin: 0px 10px 0px 10px;" >
+    <div v-for="item ,index in table " style="margin: 0px 10px 0px 10px;">
       <div :style="{borderBottom:index == table.length-1 ? '1px solid #d5d5d5':''}"
-        style="display: flex;width: 100%;border-top: 1px solid #d5d5d5;border-left: 1px solid #d5d5d5;border-right: 1px solid #d5d5d5;">
+           style="display: flex;width: 100%;border-top: 1px solid #d5d5d5;border-left: 1px solid #d5d5d5;border-right: 1px solid #d5d5d5;">
         <p
           style="flex: 42;padding: 10px;	color: #2772ff; font-size: 14px;display: flex;justify-content: flex-start;align-items: center;line-height: 20px;">
           {{item.title}}</p>
-        <divider-vertical>
-
-        </divider-vertical>
+        <div
+          style="width:1px;	background-color: #d5d5d5; font-size: 14px;display: flex;justify-content: flex-start;align-items: center;">
+        </div>
         <p
           style="flex: 30;display: flex;padding: 10px;justify-content: center;align-self: center;	color: #2772ff;font-size: 14px;line-height: 15px;">
           {{item.desc}}</p>
@@ -58,13 +57,13 @@
   import DividerVertical from '../../../components/DividerVertical1px'
 
   export default {
-    components: {XTable,DividerVertical},
+    components: {XTable, DividerVertical},
     data() {
-      return  require('./json/ScoreDeclareConfig').default
+      return require('./json/ScoreDeclareConfig').default
     },
     methonds: {},
     computed: {},
-    mounted(){
+    mounted() {
     }
   }
 </script>
