025de1eb7329bc9fb7645e056a7986ddbe27c762
guider <guider@yeah.net>
2017-12-20 19:15:56 +0800

    remove unuse files

--- src/components/NavigatorBar.vue
+++ src/components/NavigatorBar.vue
@@ -1,47 +1,47 @@
 <template>
   <tabbar slot="bottom" style="position: fixed;">
-    <tabbar-item :link="{path:'/home'}" :selected="routePath =='/home'||routePath=='/'" v-if="isP2p(routePath)">
-      <i slot="icon" class="icon iconfont icon-wxbzhuye"></i>
-      <span slot="label">排名</span>
-    </tabbar-item>
-
-    <tabbar-item :link="{path:'/deposit'}" :selected="routePath =='/deposit'" v-if="isP2p(routePath)">
-      <i slot="icon" class="icon iconfont icon-text "></i>
-      <span slot="label">存管</span>
-    </tabbar-item>
-
-    <tabbar-item :link="{path:'/trade'}" :selected="routePath =='/trade'" v-if="isP2p(routePath)">
-      <i slot="icon" class="icon iconfont icon-activity"></i>
-
-      <span slot="label">成交量</span>
-    </tabbar-item>
-    <tabbar-item :link="{path:'/me'}" :selected="routePath =='/me'" v-if="isP2p(routePath)">
-      <!--<i slot="icon" class="iconfont icon-wxbzhanghu"></i>-->
-      <i slot="icon" class="icon iconfont icon-wxbzhanghu"></i>
-      <span slot="label">我的</span>
-    </tabbar-item>
-    <!--<tabbar-item :link="{path:'/iframe'}" :selected="routePath =='/iframe'" v-if="isP2p(routePath)">-->
-      <!--<i slot="icon" class="icon iconfont icon-iconfontxingxing"></i>-->
-      <!--<span slot="label">我的</span>-->
+    <!--<tabbar-item :link="{path:'/home'}" :selected="routePath =='/home'||routePath=='/'" v-if="isP2p(routePath)">-->
+      <!--<i slot="icon" class="icon iconfont icon-wxbzhuye"></i>-->
+      <!--<span slot="label">排名</span>-->
     <!--</tabbar-item>-->
 
-    <tabbar-item :link="{path:'/sign'}" :selected="routePath =='/sign'" v-if="isSign(routePath)">
-      <img slot="icon" src="../assets/images/11.png">
-      <img slot="icon-active" src="../assets/images/1.png">
-      <span slot="label">今日签到</span>
-    </tabbar-item>
+    <!--<tabbar-item :link="{path:'/deposit'}" :selected="routePath =='/deposit'" v-if="isP2p(routePath)">-->
+      <!--<i slot="icon" class="icon iconfont icon-text "></i>-->
+      <!--<span slot="label">存管</span>-->
+    <!--</tabbar-item>-->
 
-    <tabbar-item :link="{path:'/signRanking'}" :selected="routePath =='/signRanking'" v-if="isSign(routePath)">
-      <img slot="icon" src="../assets/images/11.png">
-      <img slot="icon-active" src="../assets/images/1.png">
-      <span slot="label">排行榜</span>
-    </tabbar-item>
+    <!--<tabbar-item :link="{path:'/trade'}" :selected="routePath =='/trade'" v-if="isP2p(routePath)">-->
+      <!--<i slot="icon" class="icon iconfont icon-activity"></i>-->
 
-    <tabbar-item :link="{path:'/signScore'}" :selected="routePath =='/sign'" v-if="isSign(routePath)">
-      <img slot="icon" src="../assets/images/11.png">
-      <img slot="icon-active" src="../assets/images/1.png">
-      <span slot="label">脑容量</span>
-    </tabbar-item>
+      <!--<span slot="label">成交量</span>-->
+    <!--</tabbar-item>-->
+    <!--<tabbar-item :link="{path:'/me'}" :selected="routePath =='/me'" v-if="isP2p(routePath)">-->
+      <!--&lt;!&ndash;<i slot="icon" class="iconfont icon-wxbzhanghu"></i>&ndash;&gt;-->
+      <!--<i slot="icon" class="icon iconfont icon-wxbzhanghu"></i>-->
+      <!--<span slot="label">我的</span>-->
+    <!--</tabbar-item>-->
+    <!--&lt;!&ndash;<tabbar-item :link="{path:'/iframe'}" :selected="routePath =='/iframe'" v-if="isP2p(routePath)">&ndash;&gt;-->
+      <!--&lt;!&ndash;<i slot="icon" class="icon iconfont icon-iconfontxingxing"></i>&ndash;&gt;-->
+      <!--&lt;!&ndash;<span slot="label">我的</span>&ndash;&gt;-->
+    <!--&lt;!&ndash;</tabbar-item>&ndash;&gt;-->
+
+    <!--<tabbar-item :link="{path:'/sign'}" :selected="routePath =='/sign'" v-if="isSign(routePath)">-->
+      <!--<img slot="icon" src="../assets/images/11.png">-->
+      <!--<img slot="icon-active" src="../assets/images/1.png">-->
+      <!--<span slot="label">今日签到</span>-->
+    <!--</tabbar-item>-->
+
+    <!--<tabbar-item :link="{path:'/signRanking'}" :selected="routePath =='/signRanking'" v-if="isSign(routePath)">-->
+      <!--<img slot="icon" src="../assets/images/11.png">-->
+      <!--<img slot="icon-active" src="../assets/images/1.png">-->
+      <!--<span slot="label">排行榜</span>-->
+    <!--</tabbar-item>-->
+
+    <!--<tabbar-item :link="{path:'/signScore'}" :selected="routePath =='/sign'" v-if="isSign(routePath)">-->
+      <!--<img slot="icon" src="../assets/images/11.png">-->
+      <!--<img slot="icon-active" src="../assets/images/1.png">-->
+      <!--<span slot="label">脑容量</span>-->
+    <!--</tabbar-item>-->
   </tabbar>
 </template>
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import Trade from '@/pages/trade/Trade'
-import Deposit from '@/pages/deposit/Deposit'
-import Me from '@/pages/me/Me'
-import PlatDetail from '../pages/detail/PlatDetail.vue'
-import Search from '../pages/search/Search.vue';
-import Evaluate from '../pages/evaluate/Evaluate.vue'
-import Coupons from '../pages/coupons/Coupons.vue'
-import Attention from '../pages/attention/Attention.vue'
-import Sign from '../pages/signPages/main/Sign.vue'
-import SignRanking from '../pages/signPages/signRanking/SignRanking.vue'
-import SignScore from '../pages/signPages/signScore/SignScore.vue'
-import Qrcode from '../pages/qrcode/Qrcode.vue'
-import Shake from '../pages/game/shake/Shake.vue'
-import Test from '../pages/test/Test.vue'
-import Profile from '../pages/profile/Profile.vue'
-import FillProfile from '../pages/profile/pages/FillProfile.vue'
-import Iframe from '../pages/iframe/Iframe.vue'
-import FillPhone from '../pages/profile/pages/FillPhone.vue'
-import IncomeDetail from '../pages/me/pages/IncomeDetail.vue'
-import Withdrawal from '../pages/me/pages/Withdrawal.vue'
-import ScoreDeclare from '../pages/me/pages/ScoreDeclare.vue'
-import ScoreStore from '../pages/scoreStore/scoreStore'
 import ShuangDan from '../pages/activity/ShuangDan';
 
 Vue.use(Router);
@@ -33,203 +10,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: '前100排名',
-      component: Home,
-      meta: {
-        keepAlive: true
-      }
-    },
-    {
-      path: '/home',
-      name: '前100排名',
-      component: Home
-      ,
-      meta: {
-        keepAlive: true
-      }
-    }, {
-
-      path: '/deposit',
-      name: '存管',
-      component: Deposit
-      ,
-      meta: {
-        keepAlive: true
-      }
-
-    }, {
-      path: '/trade',
-      name: '交易量',
-      component: Trade
-      ,
-      meta: {
-        keepAlive: true
-      }
-    }, {
-      path: '/detail',
-      name: '平台详情',
-      component: PlatDetail,
-      meta: {
-        keepAlive: false,
-        navHidden: true
-      }
-    },
-    {
-      path: '/search',
-      name: '搜索',
-      component: Search,
-      meta: {
-        keepAlive: true,
-        navHidden: true
-      }
-    },
-    {
-      path: '/evaluate',
-      name: '评分',
-      component: Evaluate,
-      meta: {
-        keepAlive: false,
-        navHidden: true
-      }
-    },
-    {
-      path: '/coupons',
-      name: '我的优惠券',
-      component: Coupons,
-      meta: {
-        keepAlive: true,
-        navHidden: true
-      }
-    }, {
-      path: '/me',
-      name: '我的',
-      component: Me,
-      meta: {
-        navHidden: false
-      }
-    },
-    {
-      path: '/attention',
-      name: '我的关注',
-      component: Attention,
-      meta: {
-        keepAlive: true,
-        navHidden: true
-      }
-    },
-    {
-      path: '/sign',
-      name: '签到',
-      component: Sign,
-      meta: {
-        keepAlive: true,
-      }
-    },
-    {
-      path: '/signRanking',
-      name: '排行榜',
-      component: SignRanking,
-      meta: {
-        keepAlive: true,
-      }
-    },
-    {
-      path: '/signScore',
-      name: '脑容量',
-      component: SignScore,
-      meta: {
-        keepAlive: true,
-      }
-    },
-    {
-      path: '/qrcode',
-      name: '二维码',
-      component: Qrcode,
-      meta: {
-        navHidden: true,
-      }
-    }, {
-      path: '/test',
-      name: '测试',
-      component: Test,
-      meta: {
-        navHidden: true,
-      }
-    }
-    , {
-      path: '/shake',
-      name: '摇一摇',
-      component: Shake,
-      meta: {
-        navHidden: true,
-      }
-    }, {
-      path: '/profile',
-      name: '个人资料',
-      component: Profile,
-      meta: {
-        navHidden: true,
-      }
-    }
-    , {
-      path: '/fillPhone',
-      name: '验证手机号',
-      component: FillPhone,
-      meta: {
-        navHidden: true,
-      }
-    }, {
-      path: '/incomeDetail',
-      name: '明细',
-      component: IncomeDetail,
-      meta: {
-        keepAlive: false,
-        navHidden: true
-      }
-    }, {
-      path: '/iframe',
-      name: '内嵌',
-      component: Iframe,
-
-    }, {
-      path: '/scoreStore',
-      name: '积分活动',
-      component: ScoreStore,
-
-    }, {
-      path: '/fillProfile',
-      name: '收货地址',
-      component: FillProfile,
-      meta: {
-        navHidden: true,
-      }
-    }, {
-      path: '/withdrawal',
-      name: '提现结果',
-      component: Withdrawal,
-      meta: {
-        navHidden: true,
-      }
-    }, {
-      path: '/scoreDeclare',
-      name: '积分规则',
-      component: ScoreDeclare,
-      meta: {
-        navHidden: true,
-      }
+      redirect: '/shuangdan'
     }, {
       path: '/shuangdan',
       name: 'shuangdan',
       component: ShuangDan,
       meta: {
-        title:'双旦狂欢',
+        title: '双旦狂欢',
         navHidden: true,
       }
     }
     , {
       path: '/*',
       name: '首页',
-      component: Home
+      component: ShuangDan
     }
   ],
 
