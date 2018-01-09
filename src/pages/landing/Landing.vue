e56ac5a3f9a0991f87fbae162626f4afaeb64268
guider <guider@yeah.net>
2018-01-09 15:17:06 +0800

    landing

--- src/pages/landing/Landing.vue
+++ src/pages/landing/Landing.vue
@@ -3,17 +3,28 @@
 </template>
 
 <script>
+  import fetch from '../../api/http'
   export default {
     name: 'landing',
     components: {},
     data() {
       return {}
     },
-    methods: {},
+    methods: {
+      loadData(){
+        fetch('/table/platformDetail', {type: 'post', params: {'platid': this.$route.query.platid}})
+          .then((response) => {
+            console.log(response)
+          }).catch(function (err) {
+            console.log(err)
+        });
+      }
+    },
     computed: {},
     mounted() {
       this.$refs.root.parentNode.paddingBottom = 0;
       console.log(this.$route.query)
+      this.loadData()
     }
   }
 </script>
